abcee8b0ae1b1f856664631d99a3aff946a86c5e
Gu Junhua <jhgu@nao.cas.cn>
2020-03-06 20:25:16 +0800

    modified:   src/bin/mcmc_ana_gibbs.rs

--- src/bin/mcmc_ana_gibbs.rs
+++ src/bin/mcmc_ana_gibbs.rs
@@ -44,45 +44,23 @@ fn main(){
 
     let ntod=ptr_mat.rows();
     let nx=ptr_mat.cols();
-    let smooth_param=2.0;
-    let lp=|x: &LsVec<f64, Vec<f64>>|{
-
-        let sky:Vec<_>=x.0.iter().take(nx).cloned().collect();
-        let pps:Vec<_>=x.0.iter().skip(nx).cloned().collect();
-        logprob_ana(&sky, &pps, total_tod.as_slice().unwrap(), &ptr_mat)
-    };
-
-    let lp_grad=|x: &LsVec<f64, Vec<f64>>|{
-        let sky:Vec<_>=x.0.iter().take(nx).cloned().collect();
-        let pps:Vec<_>=x.0.iter().skip(nx).cloned().collect();
-        let (gx, gp)=logprob_ana_grad(&sky, &pps, total_tod.as_slice().unwrap(), &ptr_mat);
-        LsVec(gx.iter().chain(gp.iter()).cloned().collect::<Vec<_>>())
-    };
     //let answer=vec![0.0; answer.len()];
     let pps=vec![0.1, 0.1, 1.0/30.0, 0.0];
     let x:Vec<_>=answer.iter().chain(pps.iter()).cloned().collect();
     let mut q=LsVec(x);
-    let mut lp_value=lp(&q);
-    let mut lp_grad_value=lp_grad(&q);
 
-    let dx=LsVec(q.0.iter().map(|_|{
-        let f:f64=rng.sample(StandardNormal);
-        f*0.0001
-    }).collect::<Vec<_>>());
-
-    let diff=dx.dot(&lp_grad_value);
-    let q2=&q+&dx;
-    let lp_value2=lp(&q2);
-    println!("{} {}", diff, lp_value2-lp_value);
     
     let mut accept_cnt=0;
     let mut cnt=0;
+    let mut epsilon=0.003;
     let mut epsilon_p=0.003;
     let mut epsilon_s=0.003;
     let mut accept_cnt_p=0;
     let mut accept_cnt_s=0;
+    let mut accept_cnt=0;
     let mut cnt_p=0;
     let mut cnt_s=0;
+    let mut cnt=0;
     //let param=HmcParam::quick_adj(0.75);
     let mut param=HmcParam::new(0.75, 0.05);
 
@@ -90,7 +68,7 @@ fn main(){
         if i>100{
             param=HmcParam::slow_adj(0.75);
         }
-        if i%2==1{//sample p
+        if i%3==1{//sample p
             let sky=q.0.iter().take(nx).cloned().collect::<Vec<_>>();
             let mut q1=LsVec(q.0.iter().skip(nx).cloned().collect::<Vec<_>>());
 
@@ -115,9 +93,9 @@ fn main(){
 
             q=LsVec(sky.iter().chain(q1.iter()).cloned().collect::<Vec<_>>());
             
-            println!("{} {:.3} {:.6} {:.5}  {:?}",i, accept_cnt_p as f64/cnt_p as f64, epsilon_p, lp_value, q1.0);
+            println!("{} {:.3} {:.8} {:.5}  {:?}",i, accept_cnt_p as f64/cnt_p as f64, epsilon_p, lp_value, q1.0);
 
-        }else{
+        }else if i%3==0{
             let pps=q.0.iter().skip(nx).cloned().collect::<Vec<_>>();
             let mut q1=LsVec(q.0.iter().take(nx).cloned().collect::<Vec<_>>());
 
@@ -129,7 +107,7 @@ fn main(){
                 let (gx, gp)=logprob_ana_grad(&sky, &pps, total_tod.as_slice().unwrap(), &ptr_mat);
                 LsVec(gx)
             };
-            let mutlp_value=lp(&q1);
+            let mut lp_value=lp(&q1);
             let mut lp_grad_value=lp_grad(&q1);
 
             for j in 0..100{
@@ -142,7 +120,37 @@ fn main(){
             q=LsVec(q1.iter().chain(pps.iter()).cloned().collect::<Vec<_>>());
 
             let mean_value=q1.0.iter().sum::<f64>()/nx as f64;
-            println!("{} {:.3} {:.6} {:.5} {:e}",i, accept_cnt_s as f64/cnt_s as f64, epsilon_s, lp_value,mean_value);
+            println!("{} {:.3} {:.8} {:.5} {:e}",i, accept_cnt_s as f64/cnt_s as f64, epsilon_s, lp_value,mean_value);
+        }else{
+            let lp=|x: &LsVec<f64, Vec<f64>>|{
+
+                let sky:Vec<_>=x.0.iter().take(nx).cloned().collect();
+                let pps:Vec<_>=x.0.iter().skip(nx).cloned().collect();
+                logprob_ana(&sky, &pps, total_tod.as_slice().unwrap(), &ptr_mat)
+            };
+        
+            let lp_grad=|x: &LsVec<f64, Vec<f64>>|{
+                let sky:Vec<_>=x.0.iter().take(nx).cloned().collect();
+                let pps:Vec<_>=x.0.iter().skip(nx).cloned().collect();
+                let (gx, gp)=logprob_ana_grad(&sky, &pps, total_tod.as_slice().unwrap(), &ptr_mat);
+                LsVec(gx.iter().chain(gp.iter()).cloned().collect::<Vec<_>>())
+            };
+        
+            let mut lp_value=lp(&q);
+            let mut lp_grad_value=lp_grad(&q);
+
+            for i in 0..100{
+                let accepted=sample(&lp, &lp_grad, &mut q, &mut lp_value, &mut lp_grad_value, &mut rng, &mut epsilon, 20, &param);
+                if accepted{
+                    accept_cnt+=1;
+                }
+                cnt+=1;    
+            }
+            let mean_value=q.0.iter().take(nx).sum::<f64>()/nx as f64;
+            let ps_param=q.0.iter().skip(nx).collect::<Vec<_>>();
+
+            println!("{} {:.3} {:.8} {:.5} {:e} {:?}",i, accept_cnt_s as f64/cnt_s as f64, epsilon, lp_value,mean_value, ps_param);
         }
+
     }
 }
