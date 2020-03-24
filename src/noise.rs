2e4ca4c31097b041f5182a960bbd729d8b198261
Gu Junhua <jhgu@nao.cas.cn>
2020-03-24 22:42:03 +0800

    deleted:    src/bin/mcmc_ana.rs 	deleted:    src/bin/mcmc_ana_gibbs.rs 	deleted:    src/bin/test_ln_like.rs 	deleted:    src/bin/test_ln_like2.rs 	modified:   src/lib.rs 	deleted:    src/mcmc.rs 	deleted:    src/mcmc_func.rs 	deleted:    src/mcmc_func2.rs 	modified:   src/noise.rs

--- src/noise.rs
+++ src/noise.rs
@@ -1,6 +1,5 @@
 use crate::mcmc2d_func::ps_model as ps_model_2d;
 use crate::mcmc2d_func::{PS_E, PS_W};
-use crate::mcmc_func::ps_model;
 //use fftn::{fft2, ifft, ifft2};
 use crate::utils::{fft2, ifft2, fft, ifft};
 use ndarray::Array1;
@@ -11,57 +10,6 @@ use rand::Rng;
 use rand_distr::Distribution;
 use rand_distr::StandardNormal;
 type T=f64;
-pub fn gen_noise<U>(ntod: usize, psp: &[T], rng: &mut U, dt: T) -> Vec<T>
-where
-/*
-    T: Float
-        + NumCast
-        + FloatConst
-        + std::fmt::Debug
-        + fftn::FFTnum
-        + std::convert::From<u32>
-        + NumAssign,
-        */
-    StandardNormal: Distribution<T>,
-    U: Rng,
-{
-    let fmin = T::one() / (dt * <T as NumCast>::from(ntod).unwrap());
-    let sigma = (0..(ntod + 2) / 2)
-        .map(|i| {
-            if i > 0 {
-                Complex::<T>::new(rng.sample(StandardNormal), rng.sample(StandardNormal))
-            } else {
-                let f: T = rng.sample(StandardNormal);
-                Complex::<T>::new(f, T::zero())
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let mut psd = (0..(ntod as isize + 1) / 2)
-        .chain(-(ntod as isize) / 2..0)
-        .map(|i| {
-            let mut x = sigma[i.abs() as usize]
-                * ps_model(
-                    <T as NumCast>::from(i).unwrap() * fmin,
-                    psp[0],
-                    psp[1],
-                    psp[2],
-                    psp[3],
-                    T::from_f64(PS_W).unwrap(),
-                    T::from_f64(PS_E).unwrap(),
-                );
-            if i < 0 {
-                x = x.conj();
-            }
-            x
-        })
-        .map(|x| x.sqrt())
-        .collect::<Vec<_>>();
-
-    let mut noise = vec![Complex::<T>::new(T::zero(), T::zero()); psd.len()];
-    ifft(&mut psd, &mut noise);
-    noise.into_iter().map(|x| x.re).collect()
-}
 
 pub fn noise2d_psd(n_t: usize, n_ch: usize, psp: &[T], dt: T) -> Array2<T>
 where
@@ -108,25 +56,6 @@ where
     )
 }
 
-pub fn white1d<U>(n_t: usize, rng: &mut U, sigma: T) -> Array1<T>
-where
-/*
-    T: Float
-        + NumCast
-        + FloatConst
-        + std::fmt::Debug
-        + fftn::FFTnum
-        + std::convert::From<u32>
-        + NumAssign,*/
-    StandardNormal: Distribution<T>,
-    U: Rng,
-{
-    let mut white = Array1::<T>::zeros((n_t,));
-    for i in 0..n_t {
-        white[i] = rng.sample::<T, StandardNormal>(StandardNormal) * sigma;
-    }
-    white
-}
 
 pub fn white2d<U>(n_t: usize, n_ch: usize, rng: &mut U, sigma: T) -> Array2<T>
 where/*
